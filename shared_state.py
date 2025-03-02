7fa197eb7357cbc7b6ae82556263dfbd59a5767a
Stefany Guevara <yguevaram@unal.edu.co>
2025-03-02 17:37:21 -0500

    Update

--- shared_state.py
+++ shared_state.py
@@ -1,4 +1,32 @@
 # shared_state.py
-titulo = None
-secciones = None
-parrafos = None
+import json
+import os
+
+archive = "extracto_wikipedia.json"
+
+def cargar():
+    if os.path.exists(archive):
+        with open(archive, "r", encoding="utf-8") as file:
+            return json.load(file)
+    else:
+        return {}
+
+def guardar(estado):
+    with open(archive, "w", encoding="utf-8") as file:
+        json.dump(estado, file, indent=4, ensure_ascii=False)
+
+estado = cargar()
+
+def set_titulo(value):
+    estado["titulo"] = value
+    guardar(estado)
+    
+def get_titulo():
+    return estado.get("titulo", None)
+
+def get_parrafos():
+    return estado.get("parrafos", {})
+
+def set_parrafos(value):
+    estado["parrafos"] = value
+    guardar(estado)
