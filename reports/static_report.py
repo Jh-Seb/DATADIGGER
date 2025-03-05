892d10f253c1e33eca02f3f6845bbb814151178e
Stefany Guevara <yguevaram@unal.edu.co>
2025-03-04 21:34:28 -0500

    Update

--- reports/static_report.py
+++ reports/static_report.py
@@ -1,12 +1,15 @@
 import shared_state
 from fpdf import FPDF  
+from pathlib import Path
 import os
 import re
 
+# Función para limpiar el texto y evitar caracteres no compatibles 
 def limpiar_texto(texto):
                 texto = re.sub(r'[\u200b\u202a\u202c]', '', texto)  
                 return texto.encode('latin-1', 'ignore').decode('latin-1')
 
+# Clase personalizada para generar reportes en PDF
 class ReportPDF(FPDF):
         def header(self):
                 self.set_font("Times",'', 10)
@@ -32,23 +35,27 @@ class ReportPDF(FPDF):
                       self.multi_cell(0,10,limpiar_texto(parrafo), align='L')
                       self.ln(3)
 
+# Clase para generar el PDF con los datos obtenidos             
 class Generator_report(ReportPDF):
-      
         def __init__(self):
                 super().__init__()
                 self.set_auto_page_break(auto = True, margin = 15)
+
         def generar(self):
                 self.add_page()
                 titulo = shared_state.get_titulo()
                 self.nickname(titulo)
                 parrafos = shared_state.get_parrafos()
-
+                palabras = shared_state.get_palabras_relacionadas()
                 for seccion, parrafos in parrafos.items():
                         self.indice(seccion, parrafos)
                 filename = f'{titulo}.pdf'
-                self.output(filename)
-                os.startfile(filename)
+                download_folder = str(Path.home() / "Downloads")
+                file_path = os.path.join(download_folder, filename)
+                self.output(file_path)
+                os.startfile(file_path)
                 print('Reporte generado exitosamente')
+
 
 if __name__ == '__main__':
     reporte = Generator_report()
