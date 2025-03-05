b4c82dbb4c44ad6d0c71494dab28f43f045ee334
Stefany Guevara <yguevaram@unal.edu.co>
2025-03-04 22:19:08 -0500

    Update

--- reports/static_report.py
+++ reports/static_report.py
@@ -46,9 +46,10 @@ class Generator_report(ReportPDF):
                 titulo = shared_state.get_titulo()
                 self.nickname(titulo)
                 parrafos = shared_state.get_parrafos()
-                palabras = shared_state.get_palabras_relacionadas()
+          
                 for seccion, parrafos in parrafos.items():
                         self.indice(seccion, parrafos)
+                  
                 filename = f'{titulo}.pdf'
                 download_folder = str(Path.home() / "Downloads")
                 file_path = os.path.join(download_folder, filename)
